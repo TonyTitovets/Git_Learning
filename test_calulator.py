980e5a9c6e89f11d7e75917e06c703274df352ee
Tony Titovets <Titovets.Anton@mail.ru>
2025-04-05 16:47:45 +0300

    ТЗ

--- test_calulator.py
+++ test_calulator.py
@@ -1,5 +1,12 @@
 from calculator import Calculator
 
+
+
+# Задание
+# 1. Разработчику 1 _реализовать функцию умножение
+# 2. Разработчику 2 _реализовать функцию деление
+
+
 calc= Calculator()
 calc.sum(10,5)
 calc.print_last_res()
