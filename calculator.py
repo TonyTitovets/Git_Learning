c6b9b778b264fe72d5ff2d997d5845d04feb6b2d
Tony Titovets <Titovets.Anton@mail.ru>
2025-04-05 17:56:15 +0300

    Task- 2 - Функция деления

--- calculator.py
+++ calculator.py
@@ -1,9 +1,20 @@
+import traceback
+
 
 class Calculator:
     last_res = None
     def sum (self, n1, n2):
         self.last_res = n1 + n2
         return n1 + n2
+
+    def divid (self, n1, n2):
+        try:
+            res= n1 / n2
+            self.last_res = res
+            return res
+        except:
+            traceback.print_exc()
+
 # 1. Разработчику 1 _реализовать функцию умножение
     def multiply (self, n1, n2):
         self.last_res = n1 * n2
